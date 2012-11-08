82bb20312fa8c1b71f3dbd83df3fae8c4f2a6640
John Doe <johndoe@example.com>
2012-11-08 08:40:58 -0800

    MORE COMMIT

--- test1.rb
+++
@@ -1,6 +0,0 @@
-require "watir-webdriver"
-
-browser = Watir::Browser.new :ff
-browser.goto "http://stackoverflow.com/questions/8211006/retrieve-td-text-using-watir"
-text1 = browser.text
-puts text1
