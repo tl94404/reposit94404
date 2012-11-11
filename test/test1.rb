64d5dc2241617a73c4c52d6dfbb51c8b315c98ef
John Doe <johndoe@example.com>
2012-11-10 22:03:03 -0800

    UPDATE

--- test/test1.rb
+++ test/test1.rb
@@ -1,6 +1,8 @@
-=begin
+require "watir-webdriver"
+
 browser = Watir::Browser.new :ff
 browser.goto "http://stackoverflow.com/questions/8211006/retrieve-td-text-using-watir"
 text1 = browser.text
 puts text1
-=end
+
+
